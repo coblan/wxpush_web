d23d49796be983dd1d4df414272487ca9258f7d8
heyulin <heyulin@smalltreemedia.com>
2017-05-27 10:48:22 +0800

    部署

---
+++ src/settings/prod.py
@@ -0,0 +1,12 @@
+from base import *
+
+DATABASES = {
+    'default': {
+        'NAME': 'wxpush',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'he123811',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',        
+      },
+    }
